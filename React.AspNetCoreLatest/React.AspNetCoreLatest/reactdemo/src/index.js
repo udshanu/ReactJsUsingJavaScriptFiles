c9501809f6eb1871e53c4c1edd8fcb0a6e2b50a5
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-18 13:04:04 +0530

    Component Communication Using Context(From Child comonents to Parent component)

--- React.AspNetCoreLatest/React.AspNetCoreLatest/reactdemo/src/index.js
+++ React.AspNetCoreLatest/React.AspNetCoreLatest/reactdemo/src/index.js
@@ -3,47 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-////------------Component Communication Using Context(From Parent comonent to nested child components)--------//////////
-//In typical react application data is passed from parent component to the child component via properties.
-//But this can be difficult for 2nd types of proprties that are required by many components which are nested at different level with in an application.
-//How do we used context in react to passed the data between components placed it deffierent nesting levels.
-//Context provids a way to pass data through the components without having to pass properties from parent to child manually at every level.
-//Context provids a way to share values between component without having to explicity pass property to every level of the tree.
-//Context is primarally used when some data needs to be accessible by many components at different nesting levels.
-//When you're defining the context variable in the child component, you have to define it as 'static contextType=employeeContext' and not 'static context=employeeContext'.
-//Descendants of a Provider will re-render whenever the Provider value changes.
+////------------Component Communication Using Context(From Child comonents to Parent component)--------//////////
+//How do we update context from nested component and how do we communicate it back to the parent component.
 
-const employeeContext=React.createContext();
+const employeeContext = React.createContext({
+    data:'',
+    changeEmployeeInfo:()=>{}
+});
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            Id: 101,
-            Name: "Roshan",
-            Location: "Kurunegala",
-            Salary: 15000
+            data: {
+                Id: 101,
+                Name: "Roshan",
+                Location: "Kurunegala",
+                Salary: 15000
+            },
+            changeEmployeeInfo: this.updateEmployeeDetails
         };
     }
 
-    changeEmployeeData = () => {
-        this.setState({
-            Id: 102
-        });
+    updateEmployeeDetails = () => {
+        this.setState({ data: { Id: 102}});
     }
 
     render() {
         return <div>
             <h2>Welcome to App Component..</h2>
             <p>
-                <label>Employee Id: <b>{this.state.Id}</b></label>
+                <label>Employee Id: <b>{this.state.data.Id}</b></label>
             </p>
             <employeeContext.Provider value={this.state}>
                 <Employee></Employee>
             </employeeContext.Provider>
-            <p>
-                <button onClick={this.changeEmployeeData}>Update</button>
-            </p>
         </div>
     }
 }
@@ -56,22 +50,10 @@ class Employee extends React.Component {
         return <div>
             <h2>Welcome to Employee Component..</h2>
             <p>
-                <label>Employee Id: <b>{this.context.Id}</b></label>
+                <label>Employee Id: <b>{this.context.data.Id}</b></label>
             </p>
-            <Salary></Salary>
-        </div>
-    }
-}
-
-class Salary extends React.Component {
-    //static context = employeeContext;
-    static contextType = employeeContext;
-
-    render() {
-        return <div>
-            <h2>Welcome to Salary Component..</h2>
             <p>
-                <label>Employee Id: <b>{this.context.Id}</b></label>
+                <button onClick={this.context.changeEmployeeInfo}>Update</button>
             </p>
         </div>
     }
