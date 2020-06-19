d610b78d6ae82e313eae6c995efac42de69e72a7
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-19 13:14:54 +0530

    Understanding FORMS validation in React(In manually)

--- React.AspNetCoreLatest/React.AspNetCoreLatest/reactdemo/src/index.js
+++ React.AspNetCoreLatest/React.AspNetCoreLatest/reactdemo/src/index.js
@@ -4,24 +4,31 @@ import { useFormik, Formik } from 'formik';
 import './index.css';
 
 
-////------------Create FORMS Using Formik library in React--------//////////
-//In React state of this input elements is typically kept in state property of the component, and only update it with setState method.
-//Input Form element controll by react in this fasion is called as "Controlled Inputs" or "Controlled Components".
-//The alternative is "Uncontrolled Inputs" or "Uncontrolled Components" where form data is handled by the DOM itself.
-//We will use "ref" to get value of this input and performe operation of this data.
-//Uncontrolled inputs are simplest way to implement the form inputs but they are not powerful enough to build real world applictions.
-//We can get all property values by using "Spred Oparator" 
-//React do not provide some features by default.(validations, keep track of visited fields, handle form submition, etc..)
+////------------Understanding FORMS validation in React(In manually)--------//////////
 
-//We use "Formik" labrary for build powerfull applications in React.
-//Formik also build on the same principles of control components and managing states.
+const validateEmployee = empData => {
+    const errors = {};
 
-//What is "Formik"?
-//Small group of React components and functions used for building forms in React and React Native.
-//This helps 3 most important activities.
-//1. Get values from form
-//2. Handling validation of form data.
-//3. Handiling form submition.
+    if (!empData.Name) {
+        errors.Name = 'Please enter employee name.';
+    }
+    else if (empData.Name.length > 20) {
+        errors.Name = 'Employee name should not exceed 20 characters.';
+    }
+
+    if (!empData.Location) {
+        errors.Location = 'Please enter employee location.';
+    }
+
+    if (!empData.EmailId) {
+        errors.EmailId = 'Please enter email id.';
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(empData.EmailId)) {
+        errors.EmailId = 'Invalid email address.';
+    }
+
+    return errors;
+}
 
 const EmployeeComponent = () => {
     const formik = useFormik({
@@ -29,8 +36,10 @@ const EmployeeComponent = () => {
             Id: '',
             Name: '',
             Location: '',
-            Salary: ''
+            Salary: '',
+            EmailId: ''
         },
+        validate: validateEmployee,
         onSubmit: values => {
             alert(JSON.stringify(values));
         }
@@ -46,15 +55,22 @@ const EmployeeComponent = () => {
                 </p>
                 <p>
                     <label htmlFor="Name">Employee Name:</label>
-                    <input type="text" name="Name" id="Name" value={formik.values.Name} onChange={formik.handleChange}></input>
+                    <input type="text" name="Name" id="Name" value={formik.values.Name} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.Name && formik.errors.Name ? <span style={{ color: 'red' }}>{formik.errors.Name}</span> : null}
                 </p>
                 <p>
                     <label htmlFor="Location">Employee Location:</label>
-                    <input type="text" name="Location" id="Location" value={formik.values.Location} onChange={formik.handleChange}></input>
+                    <input type="text" name="Location" id="Location" value={formik.values.Location} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.Location && formik.errors.Location ? <span style={{ color: 'red' }}>{formik.errors.Location}</span> : null}
                 </p>
                 <p>
                     <label htmlFor="Salary">Employee Salary:</label>
                     <input type="text" name="Salary" id="Salary" value={formik.values.Salary} onChange={formik.handleChange}></input>
+                </p>
+                <p>
+                    <label htmlFor="EmailId">Employee Email Id:</label>
+                    <input type="text" name="EmailId" id="EmailId" value={formik.values.EmailId} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.EmailId && formik.errors.EmailId ? <span style={{ color: 'red' }}>{formik.errors.EmailId}</span> : null}
                 </p>
                 <button type="submit">Create</button>
             </form>
